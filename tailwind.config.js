a582fa537db435bc1f3bf57be237a2bd0a9e28f2
rumpapl <rumpakucse14@gmail.com>
2021-11-29 21:28:04 +0600

    created custom bg-color for footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        footer: {
+          DEFAULT: '#20375c',
+        }
+      }
+    },
   },
   variants: {
     extend: {},
